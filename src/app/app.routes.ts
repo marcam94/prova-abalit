6ec3a01cb2536c7fb1d5f39060f5c9c8c92f43f9
marc <marcalbiol48@gmail.com>
2024-07-04 10:50:54 +0200

    feat(layout): diseño de la pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LayoutComponent} from "./shared/components/layout/layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'home'
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/task-list/task-list.component').then(x => x.TaskListComponent)
+      }
+    ]
+  }
+];
